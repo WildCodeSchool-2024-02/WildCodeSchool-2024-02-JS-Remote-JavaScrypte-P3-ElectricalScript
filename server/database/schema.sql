3ba0dbb53ae7f3a5b16de3ab125bc48fcd177864
Pierre Juppin <pierre.juppin@gmail.com>
2024-06-20 11:59:45 +0200

    fix: change database to french

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE role (
     role VARCHAR(80)
 );
 
-CREATE TABLE socket (
-    socket_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE prise (
+    prise_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     type_prise VARCHAR(80) NOT NULL,
     puiss_max INT NOT NULL
 );
@@ -20,50 +20,50 @@ CREATE TABLE station (
     nbre_pdc INT,
     accessibilite VARCHAR(80),
     code_insee_commune INT,
-    socket_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+    prise_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
 );
 
-CREATE TABLE point(
-    point_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE borne(
+    borne_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     puiss_max FLOAT,
     reservation VARCHAR(80),
     observations VARCHAR(255),
     station_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (station_id) REFERENCES station (station_id),
-    socket_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+    prise_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
 );
 
-CREATE TABLE car_type (
-    car_type_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    brand VARCHAR(80),
-    model VARCHAR(80),
+CREATE TABLE voiture (
+    voiture_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    marque VARCHAR(80),
+    modele VARCHAR(80),
     image VARCHAR(255),
-    socket_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+    prise_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
 );
 
-CREATE TABLE users (
-    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
+CREATE TABLE utilisateurs (
+    utilisateur_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(80) NOT NULL,
+    mdp VARCHAR(80) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (role_id) REFERENCES role (role_id),
-    car_type_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (car_type_id) REFERENCES car_type (car_type_id)
+    voiture_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (voiture_id) REFERENCES voiture (voiture_id)
 );
 
 CREATE TABLE reservation (
     reservation_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    status VARCHAR(80),
-    price FLOAT,
-    start_date DATE,
-    end_date DATE,
-    point_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (point_id) REFERENCES point(point_id),
-    user_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    statut VARCHAR(80),
+    prix FLOAT,
+    date_debut DATE,
+    date_fin DATE,
+    borne_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (borne_id) REFERENCES borne (borne_id),
+    utilisateur_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (utilisateur_id)
 );
