169ba2c948b3b87462215a90b9287d7e98a71d1f
Pierre Juppin <pierre.juppin@gmail.com>
2024-06-20 15:23:58 +0200

    fix: change database for match csv file

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,67 +3,65 @@ CREATE TABLE role (
     role VARCHAR(80)
 );
 
-CREATE TABLE prise (
-    prise_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    type_prise VARCHAR(80) NOT NULL,
-    puiss_max INT NOT NULL
+CREATE TABLE socket (
+    socket_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    socket_type VARCHAR(80) NOT NULL,
+    power_charge INT NOT NULL
 );
 
 CREATE TABLE station (
     station_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    n_station VARCHAR(255) NOT NULL,
-    n_enseigne VARCHAR(255) NOT NULL,
-    ad_station VARCHAR(255) NOT NULL,
-    ylatitude FLOAT,
-    xlongitude FLOAT,
-    geo_point_borne FLOAT,
-    nbre_pdc INT,
-    accessibilite VARCHAR(80),
-    code_insee_commune INT,
-    prise_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
+    name VARCHAR(80) NOT NULL,
+    brand VARCHAR(80) NOT NULL,
+    adress VARCHAR(155) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    point_number INT,
+    position FLOAT NOT NULL,
+    accessibility VARCHAR(80),
+    postal_code VARCHAR(25) NOT NULL,
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
 );
 
-CREATE TABLE borne(
-    borne_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    puiss_max FLOAT,
+CREATE TABLE point(
+    point_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    power FLOAT,
     reservation VARCHAR(80),
     observations VARCHAR(255),
     station_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (station_id) REFERENCES station (station_id),
-    prise_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
 );
-
-CREATE TABLE voiture (
-    voiture_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    marque VARCHAR(80),
-    modele VARCHAR(80),
+CREATE TABLE car_type (
+    car_type_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    brand VARCHAR(80) NOT NULL,
+    model VARCHAR(80) NOT NULL,
     image VARCHAR(255),
-    prise_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (prise_id) REFERENCES prise (prise_id)
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
 );
 
-CREATE TABLE utilisateurs (
-    utilisateur_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    nom VARCHAR(255) NOT NULL,
+CREATE TABLE user (
+    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    mdp VARCHAR(80) NOT NULL,
+    password VARCHAR(80) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (role_id) REFERENCES role (role_id),
-    voiture_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (voiture_id) REFERENCES voiture (voiture_id)
+    car_type_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (car_type_id) REFERENCES car_type (car_type_id)
 );
-
 CREATE TABLE reservation (
     reservation_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    statut VARCHAR(80),
-    prix FLOAT,
-    date_debut DATE,
-    date_fin DATE,
-    borne_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (borne_id) REFERENCES borne (borne_id),
-    utilisateur_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (utilisateur_id)
+    status VARCHAR(80),
+    price FLOAT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    point_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (point_id) REFERENCES point(point_id),
+    user_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
