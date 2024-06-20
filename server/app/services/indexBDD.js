8b4afb7195ffd87f1494a099a25c291e80c10079
Chaymae El Hayani <layani.chay@gmail.com>
2024-06-20 16:08:54 +0200

    feat: read csv file with cvs parse package

---
+++ server/app/services/indexBDD.js
@@ -0,0 +1,44 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const path = require("path");
+
+const results = [];
+
+const filePath = path.join(__dirname, "bornes-irve.csv");
+
+fs.createReadStream(filePath)
+  .pipe(csv({ separator: ";" }))
+  .on("data", (data) => {
+    const modifiedData = { ...data }; 
+    
+    delete modifiedData.n_amenageur;
+    delete modifiedData.n_operateur;
+    delete modifiedData.id_station;
+    delete modifiedData.code_insee;
+    delete modifiedData.id_pdc;
+    delete modifiedData.acces_recharge;
+    delete modifiedData.date_maj;
+    delete modifiedData.source;
+    delete modifiedData.region;
+    delete modifiedData.departement;
+
+    const renameData = {
+      brand: data.n_enseigne,
+      name: data.n_station,
+      adress: data.ad_station,
+      longitude: data.xlongitude,
+      latitude: data.ylatitude,
+      points_number: data.nbre_pdc,
+      power: data.puiss_max,
+      socket_type: data.type_prise,
+      accessibility: data.accessibilite,
+      observations: data.observations,
+      position: data.geo_point_borne,
+      postal_code: data.code_insee_commune
+    };
+
+    results.push(renameData);
+  })
+  .on("end", () => {
+  
+  });
