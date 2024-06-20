f6fb85ca4bfe0abff6bb899d481dfec9c40a8330
Pierre Juppin <pierre.juppin@gmail.com>
2024-06-20 11:15:36 +0200

    fix: change property table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,69 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE role (
+    role_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    role VARCHAR(80)
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE socket (
+    socket_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    type_prise VARCHAR(80) NOT NULL,
+    puiss_max INT NOT NULL
+);
+
+CREATE TABLE station (
+    station_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    n_station VARCHAR(255) NOT NULL,
+    n_enseigne VARCHAR(255) NOT NULL,
+    ad_station VARCHAR(255) NOT NULL,
+    ylatitude FLOAT,
+    xlongitude FLOAT,
+    geo_point_borne FLOAT,
+    nbre_pdc INT,
+    accessibilite VARCHAR(80),
+    code_insee_commune INT,
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+);
+
+CREATE TABLE point(
+    point_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    puiss_max FLOAT,
+    reservation VARCHAR(80),
+    observations VARCHAR(255),
+    station_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (station_id) REFERENCES station (station_id),
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+);
+
+CREATE TABLE car_type (
+    car_type_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    brand VARCHAR(80),
+    model VARCHAR(80),
+    image VARCHAR(255),
+    socket_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (socket_id) REFERENCES socket (socket_id)
+);
+
+CREATE TABLE users (
+    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(80) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    car_type_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (car_type_id) REFERENCES car_type (car_type_id)
+);
+
+CREATE TABLE reservation (
+    reservation_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    status VARCHAR(80),
+    price FLOAT,
+    start_date DATE,
+    end_date DATE,
+    point_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (point_id) REFERENCES point(point_id),
+    user_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
