b6218c33a3d990db0d169d31722e8ae438be57e4
Chaymae El Hayani <layani.chay@gmail.com>
2024-06-26 16:25:18 +0200

    feat: station table seeding

---
+++ server/database/fixtures/StationSeeder.js
@@ -0,0 +1,29 @@
+const stations = require("../data/stationsinf.json");
+const AbstractSeeder = require("./AbstractSeeder");
+
+class StationSeeder extends AbstractSeeder {
+  constructor() {
+    super({ table: "station", truncate: true });
+  }
+
+   run() {
+      stations.forEach((station, index) => {
+        this.insert({
+          name: station.name,
+          brand: station.brand,
+          address: station.address,
+          latitude: station.latitude,
+          longitude: station.longitude,
+          point_number: station.point_number,
+          position: station.position,
+          socket_type: station.socket_type,
+          power: station.power,
+          accessibility: station.accessibility,
+          postal_code: station.postal_code,
+          refName: `station_${index + 1}`,
+        });
+      });
+  }
+}
+
+module.exports = StationSeeder;
