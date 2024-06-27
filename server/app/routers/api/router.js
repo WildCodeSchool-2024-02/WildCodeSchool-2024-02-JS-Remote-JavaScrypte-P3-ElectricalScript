477079ef0b756736121727864242835c5fe96b48
Chaymae El Hayani <layani.chay@gmail.com>
2024-06-27 10:28:51 +0200

    fix: solid principle applied to multer file upload, parsing and routing

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,33 +1,13 @@
 const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
 
-const upload = multer({ dest: "uploads/" });
 const router = express.Router();
+
 
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-/**
- * TEST UPLOAD
- */
-
-router.post("/upload", upload.single("station"), (req, res) => {
-
-  /**
-   * Je vais récupérer le ficher présent dans req.file, et le mettre dans le dossier `uploads`
-   */
-
-  fs.rename(
-    `uploads/${req.file.filename}`,
-    `uploads/bornes-irve.csv`,
-    (err) => {
-      if (err) throw err;
-      res.status(201).json({ message: "Le fichier a bien été téléversé" });
-    }
-  );
-});
+router.post("/upload, UploadFile");
 
 const carRouter = require("./car/router");
 
