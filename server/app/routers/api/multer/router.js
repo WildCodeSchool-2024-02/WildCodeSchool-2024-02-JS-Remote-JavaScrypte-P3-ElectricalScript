f14d5f3ce01ab9018247653d9432ccd3b252b77e
Chaymae El Hayani <layani.chay@gmail.com>
2024-06-27 09:36:36 +0200

    Revert "fix: solid_principle applied to the multer config"
    This reverts commit 95e35d29c0470c4cb3ec5e7c8e1a50a79b7cf465.

--- server/app/routers/api/multer/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const multerMiddleware = require("../../../middleware/multerMiddleware"); 
-
-router.post("/upload", multerMiddleware);
-
-module.exports = router;
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
 
+const upload = multer({ dest: "uploads/" });
 const router = express.Router();
 
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+
+/**
+ * TEST UPLOAD
+ */
+
+router.post("/upload", upload.single("station"), (req, res) => {
+
+  /**
+   * Je vais récupérer le ficher présent dans req.file, et le mettre dans le dossier `uploads`
+   */
+
+  fs.rename(
+    `uploads/${req.file.filename}`,
+    `uploads/bornes-irve.csv`,
+    (err) => {
+      if (err) throw err;
+      res.status(201).json({ message: "Le fichier a bien été téléversé" });
+    }
+  );
+});
+
 const carRouter = require("./car/router");
 
 router.use("/car", carRouter);
@@ -17,10 +41,9 @@ const usersRouter = require("./users/router");
 
 router.use("/users", usersRouter);
 
+const stationRouter = require("./station/router");
 
-const multerRouter = require("./multer/router");
-
-router.use("/upload", multerRouter);
+router.use("/station", stationRouter);
 
 /* ************************************************************************* */
 
