fbddadd226a635aa478014b7046eab83fd72d07f
Chaymae El Hayani <layani.chay@gmail.com>
2024-07-11 17:31:12 +0200

    fix: minor css changes in the sidebar

--- client/src/components/admin/SideBar.jsx
+++ client/src/components/admin/SideBar.jsx
@@ -18,51 +18,49 @@ function SideBar() {
             </div>
           </div>
 
-          {/* Voitures Button */}
           <div className="flex items-center space-x-2">
             <NavLink to="/cars" className="w-full">
               <button
                 type="button"
-                className="w-full rounded-lg border border-white py-2 px-4 hover:bg-white hover:text-black transition duration-300 text-left"
+                className="w-full rounded-lg border border-white py-2 px-4"
               >
                 Voitures
               </button>
             </NavLink>
           </div>
 
-          {/* Utilisateurs Button */}
+          
           <div className="flex items-center space-x-2">
             <button
               type="button"
-              className="rounded-lg border border-white py-2 px-4 hover:bg-white hover:text-black transition duration-300"
+              className="rounded-lg border border-white py-2 px-4 "
             >
               Utilisateurs
             </button>
           </div>
 
-          {/* Informations Button */}
+          
           <div className="flex items-center space-x-2">
             <NavLink to="/admin" className="w-full">
               <button
                 type="button"
-                className="w-full rounded-lg border border-white py-2 px-4 hover:bg-white hover:text-black transition duration-300 text-left"
+                className="w-full rounded-lg border border-white py-2 px-4"
               >
                 Informations
               </button>
             </NavLink>
           </div>
 
-          {/* Stations Button */}
+        
           <div className="flex items-center space-x-2">
             <button
               type="button"
-              className="rounded-lg border border-white py-2 px-4 hover:bg-white hover:text-black transition duration-300"
+              className="rounded-lg border border-white py-2 px-4 "
             >
               Stations
             </button>
           </div>
 
-          {/* Logo */}
           <img src={logo} alt="logo geocode" className="rounded-full w-20 h-20" />
         </div>
       </div>
