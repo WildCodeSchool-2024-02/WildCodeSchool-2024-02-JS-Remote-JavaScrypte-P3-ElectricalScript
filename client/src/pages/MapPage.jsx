e84fb79f4acf1da116b99171ff2854b6f63539b7
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-07-22 19:33:31 +0200

    feat: create user profil

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,13 +1,5 @@
-import { Link } from "react-router-dom";
 import MapComponent from "../components/map/MapComponent";
 
 export default function MapPage() {
-  return (
-    <div>
-      <Link to="/registerCar" className="text-white">
-        GOGOGO
-      </Link>
-      <MapComponent />;
-    </div>
-  );
+  return <MapComponent />;
 }
